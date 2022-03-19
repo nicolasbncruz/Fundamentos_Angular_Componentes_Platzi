887ab4ec375783b2d487508a7dcdd844361adbae
Nicolas Benjamin Cruz Carpio <nicolasbncruz@hotmail.com>
2022-03-19 10:23:02 -0500

    fundamentos de angular componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.webp'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.webp'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.webp'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.webp'
+    }
+  ];
+
+  onLoaded(img: string){
+    console.log('log padre : ', img);
+  }
+
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,8 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
 
-  @Input() img: string = 'valor init';
-
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter<string>();
   imageDefault = 'https://www.w3schools.com/howto/img_avatar2.png'
 
   constructor() { }
@@ -20,6 +20,16 @@ export class ImgComponent implements OnInit {
     this.img = this.imageDefault;
   }
 
+  imgLoaded(){
+    console.log('log hijo');
+    //console.log(this.img);
+    this.loaded.emit(this.img);//se va al padre mediante Output
+
+  }
+
 }
 
 
+/**
+ https://www.w3schools.com/howto/img_avatar.png
+ */
